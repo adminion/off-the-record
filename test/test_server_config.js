e89d54c1befeb8e657cbb73e4c69e3d5e77e9ac3
Jeff Harris <techjeffharris@gmail.com>
2016-04-16 15:05:28 -0700

    finish server config tests

--- test/test_server_config.js
+++ test/test_server_config.js
@@ -82,10 +82,144 @@ module.exports = function () {
             })
           })
 
+          describe('#maxAge', function () {
+            it('is a Number greater than -1', function () {
+              cookie.maxAge.should.be.a.Number
+              cookie.maxAge.should.be.above(-1)
+            })
+          })
+
+        })
+
+        describe('#hash', function () {
+          let hash = session.hash
+
+          it('is a plain old javascript object', function () {
+            hash.should.be.an.Object
+          })
+
+          describe('#salt', function () {
+            it('is a String', function () {
+              hash.salt.should.be.a.String
+            })
+          })
+        })
+
+        describe('#key', function () {
+          let key = session.key
+
+          it('is a String', function () {
+            key.should.be.a.String
+          })
+        })
+
+        describe('#resave', function () {
+          let resave = session.resave
+
+          it('is a boolean', function () {
+            ('boolean' === typeof resave).should.be.true
+          })
+        })
+
+        describe('#saveUninitialized', function () {
+          let saveUninitialized = session.saveUninitialized
+
+          it('is a boolean', function () {
+            ('boolean' === typeof saveUninitialized).should.be.true
+          })
+        })
+
+        describe('#secret', function () {
+          let secret = session.secret
+
+          it('is a String', function () {
+            secret.should.be.a.String
+          })
+        })
+
+      })
+
+      describe('#ssl', function () {
+        let ssl = http.ssl
+
+        it('is a plain old javascript object', function () {
+          ssl.should.be.an.Object
+        })
+
+        describe('#cert', function () {
+          it('is a string', function () {
+            ssl.cert.should.be.a.String
+          })
+        })
+
+        describe('#key', function () {
+          it('is a string', function () {
+            ssl.key.should.be.a.String
+          })
+        })
+
+        describe('#prefix', function () {
+          it('is a string', function () {
+            ssl.prefix.should.be.a.String
+          })
+        })
+
+
+      })
+
+    })
+
+    describe('#mongoose', function () {
+      let mongoose = config.mongoose
+
+      it('is a plain old javascript object', function () {
+        mongoose.should.be.an.Object
+      })
+
+      describe('#conversationCollectionName', function () {
+        it('is a string', function () {
+          mongoose.conversationCollectionName.should.be.a.String
         })
       })
 
-      describe('#ssl', function () {})
+      describe('#conversationModelName', function () {
+        it('is a string', function () {
+          mongoose.conversationModelName.should.be.a.String
+        })
+      })
+
+      describe('#options', function () {
+        let options = mongoose.options
+
+        it('is a plain old javascript object', function () {
+          options.should.be.an.Object
+        })
+
+        describe('#auto_reconnect', function () {
+          it('is a boolean', function () {
+            ('boolean' === typeof options.auto_reconnect).should.be.true
+          })
+        })
+      })
+
+      describe('#personCollectionName', function () {
+        it('is a string', function () {
+          mongoose.personCollectionName.should.be.a.String
+        })
+      })
+
+      describe('#personModelName', function () {
+        it('is a string', function () {
+          mongoose.personModelName.should.be.a.String
+        })
+      })  
+
+      describe('#uri', function () {
+        it('is a string', function () {
+          mongoose.uri.should.be.a.String
+        })
+      })
+
 
     })
 
